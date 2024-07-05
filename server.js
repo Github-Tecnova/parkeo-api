97809947aa4494ca17c31593805b50a1056f3803
maelclou11 <maelclou11@hotmail.com>
2024-07-05 14:16:54 -0400

    route test

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
